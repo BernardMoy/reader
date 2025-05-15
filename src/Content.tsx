e1b68b47af1ff9b8869e0c47da8c7321f19b5c10
BernardMoy <bernardmoy3@gmail.com>
2025-05-16 00:19:44 +0100

    3 textfield number inputs for the WPM and duration

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,5 +1,5 @@
-import { TextField } from "@mui/material";
-import { MIN_TEXTFIELD_HEIGHT } from "./Values";
+import { Box, TextField } from "@mui/material";
+import { CONTENT_MARGIN, MIN_TEXTFIELD_HEIGHT, TITLE_MARGIN } from "./Values";
 import type React from "react";
 import type { SetStateAction } from "react";
 
@@ -10,24 +10,63 @@ export interface Props {
 
 export default function Content({ text, setText }: Props) {
   return (
-    // The main text field for copy pasting the paragraph
-    <TextField
-      id="main-textfield"
-      label="Text"
-      variant="outlined"
-      multiline={true}
-      value={text}
-      onChange={(text) => setText(text.target.value)}
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={TITLE_MARGIN}
       sx={{ width: "100%" }}
-      slotProps={{
-        input: {
-          sx: {
-            "& textarea": {
-              minHeight: MIN_TEXTFIELD_HEIGHT, // set the min height for the textfield by changing the textarea
+    >
+      {/* The main textfield for entering the paragraph */}
+      <TextField
+        id="main-textfield"
+        label="Text"
+        placeholder="Enter or paste your paragraph here..."
+        variant="outlined"
+        multiline={true}
+        value={text}
+        onChange={(text) => setText(text.target.value)}
+        slotProps={{
+          input: {
+            sx: {
+              "& textarea": {
+                minHeight: MIN_TEXTFIELD_HEIGHT, // set the min height for the textfield by changing the textarea
+              },
             },
           },
-        },
-      }}
-    />
+        }}
+      />
+
+      {/* The fields for setting the start wpm, end wpm and time */}
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="start"
+        gap={CONTENT_MARGIN}
+      >
+        <TextField
+          id="from-textfield"
+          label="From WPM"
+          variant="outlined"
+          type="number"
+          multiline={false}
+        />
+
+        <TextField
+          id="to-textfield"
+          label="To WPM"
+          variant="outlined"
+          type="number"
+          multiline={false}
+        />
+
+        <TextField
+          id="duration-textfield"
+          label="Duration"
+          variant="outlined"
+          type="number"
+          multiline={false}
+        />
+      </Box>
+    </Box>
   );
 }
