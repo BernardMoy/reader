364a4cb8660c5ea1af3832f4abc750c1dd04b7d6
BernardMoy <bernardmoy3@gmail.com>
2025-05-26 21:13:56 +0100

    The split function now matches one or more spaces

--- src/split.tsx
+++ src/split.tsx
@@ -1,6 +1,6 @@
 // a function to split a paragraph into words
 export default function split(text: String): String[] {
-  const wordList = text.split(/ |\n/); // regex to split
+  const wordList = text.split(/ +|\n/); // regex to split
 
   return wordList;
 }
